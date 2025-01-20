2f80029462f3d97ac9795c9d72c47d4a3dd3d12b
box3-galen-nv <145845206+box3-galen-nv@users.noreply.github.com>
2025-01-20 12:39:06 +0800

    Initial commit

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-console.log('变量加载')
-let doc_Html = Array.from(document.getElementsByTagName('html'))
-let doc_BlockTable = document.getElementById('block-table')
-let doc_Cursor = document.getElementById('cursor')
-
-console.log('处理依赖变量')
 import * as api from './api.js'
-import * as start from './mod/code/start.js'
+import * as start from './mod/code/main.js'
 
-let currentBlockTable = api.getCurrentBlockTable()
-const doc_Cursor_Img = new Image()
-doc_Cursor_Img.src = api.getMouseStyle()
-doc_Cursor_Img.onload = () => {
-    const doc_Cursor_Img_W = doc_Cursor_Img.width
-    const doc_Cursor_Img_H = doc_Cursor_Img.height
-}
 
-console.log('数据加载完成')
 
-//鼠标样式
-doc_Cursor.src = api.getMouseStyle()
-doc_Cursor.style.position = `fixed`
-doc_Cursor.style.scale = `0.5`
+class Doc {
+    constructor() {
+        this.boxTable = document.getElementById('box-table')
+        this.blockTable = document.getElementById('block-table')
+        this.cursor = document.getElementById('cursor')
+        this.Cursor = class {
+            constructor() {
+                this.img = new Image()
+                this.img.src = api.getMouseStyle()
+                this.img.onload = () => {
+                    this.pos= {'w': this.img.width, 'h': this.img}
+                }
+            }
+        }
+    }
+} console.log('数据加载完成')
+
+
+
+
 
 document.addEventListener('mousemove', function(event) {
     const mouseX = event.clientX
     const mouseY = event.clientY
-    doc_Cursor.style.left = `${mouseX+doc_Cursor}px`
-    doc_Cursor.style.top = `${mouseY}px`
+    doc_Cursor.style.left = `${mouseX - doc_Cursor_Img_Pos['h'] / 2}px`
+    doc_Cursor.style.top = `${mouseY - doc_Cursor_Img_Pos['w'] / 2}px`
 })
 
 //积木栏显示
